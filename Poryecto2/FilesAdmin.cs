d649275c12a1578ab5a9723183b421c87aab844b
RodrigoMontero1998 <46728177+RodrigoMontero1998@users.noreply.github.com>
2022-05-23 11:34:12 -0600

    Agregar archivos de proyecto.

---
+++ Poryecto2/FilesAdmin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Spire.Xls;
+
+namespace Poryecto2
+{
+    class FilesAdmin
+    {
+        public string createFolder(string dir, string nameFolder)
+        {
+            System.IO.Directory.CreateDirectory(dir + "\\" + nameFolder);
+            return dir + "\\" + nameFolder;
+        }
+        public void moveFile(string origen, string nameFile, string destino)
+        {
+            if (!System.IO.File.Exists(origen + "\\"))
+            {
+                System.IO.File.Move(origen + "\\" + nameFile, destino + "\\" + nameFile);
+            }
+        }
+
+        public string consolidar(string dir, string[] listFiles){
+            if (listFiles.Length != 0){
+                List<string> inputFiles = new List<string>();
+                foreach (string item in listFiles){
+                    if (item.Contains(".xls")){
+                        inputFiles.Add(item);
+                    }
+                }
+                if (inputFiles.Count > 0){
+                    Workbook newWorkbook = new Workbook();
+                    newWorkbook.Worksheets.Clear();
+                    Workbook tempWorkbook = new Workbook();
+                    foreach (string file in inputFiles)
+                    {
+                        tempWorkbook.LoadFromFile(file);
+                        foreach (Worksheet sheet in tempWorkbook.Worksheets)
+                        {
+                            newWorkbook.Worksheets.AddCopy(sheet, WorksheetCopyType.CopyAll);
+                        }
+                    }
+                    newWorkbook.SaveToFile(dir + "\\ArchivoMaestro.xlsx", ExcelVersion.Version2013);
+                }
+                return dir + "\\ArchivoMaestro.xlsx";
+            }
+            return "";
+        }
+    }
+}
---
+++ Poryecto2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Poryecto2
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args){
+            if (args.Length == 0){
+                FolderBrowserDialog fbd = new FolderBrowserDialog();
+                if (fbd.ShowDialog() == DialogResult.OK){
+                    string dir = fbd.SelectedPath;
+                    realizarTodo(dir);
+                    Console.WriteLine("Se consolido con Exito\n");
+                }
+            }else{
+                string dir = args[0];
+                realizarTodo(dir);
+                Console.WriteLine("Se consolido con Exito\n");
+            }
+            Console.WriteLine("El programa termino\n");
+        }
+
+        static void realizarTodo(string dir){
+            FilesAdmin f = new FilesAdmin();
+            string aux = f.consolidar(dir, Directory.GetFiles(dir));
+            if(aux != "") { 
+                foreach (string Item in Directory.GetFiles(dir)){
+                    FileInfo fi = new FileInfo(Item);
+                    moveFiles(dir,fi.Name);
+                }
+            }
+            else{
+                Console.WriteLine("No se ejecuto con exito\n");
+            }
+        }
+
+        static void moveFiles(string dir,string nameFile)
+        {
+            if(nameFile != "ArchivoMaestro.xlsx")
+            {
+                FilesAdmin f = new FilesAdmin();
+                if (nameFile.Contains(".xls"))
+                {
+                    string destino = f.createFolder(dir, "Procesado");
+                    f.moveFile(dir, nameFile, destino);
+
+                }
+                else
+                {
+                    string destino = f.createFolder(dir, "No aplicable");
+                    f.moveFile(dir, nameFile, destino);
+                }
+            }
+           
+        }
+    }
+
+    
+}
